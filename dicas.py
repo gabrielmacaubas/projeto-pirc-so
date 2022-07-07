d01ff3f6d4fe30b4ccfa0408b05099db1c23f865
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-07-07 13:49:26 -0300

    Update dicas.py

--- dicas.py
+++ dicas.py
@@ -2,5 +2,8 @@ dicas = {
     "bolo": ("tem de chocolate", "tem de cenoura", "tem de laranja"),
     "cofre": ("dinheiro", "senha", "porquinho"),
     "gorro": ("inverno", "lã", "papai noel usa"),
-    "bingo": ("tem número", "tem bolinha", "globo")
+    "bingo": ("tem número", "tem bolinha", "globo"),
+    "chave": ("tem braço", "tem inglesa", "fechadura"),
+    "balança": ("libra", "justiça", "tem no açougue"),
+    "gangorra": ("tem no parque", "brinquedo", "sobe e desce")
 }
